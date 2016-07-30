f53957ec7b9ccdca5c78e3e976d777918951bd1e
bw-giraffe <theletterrsquared@gmail.com>
2016-07-29 20:47:05 -0700

    set up ajax requests

--- app/assets/javascripts/vizualization.js
+++ app/assets/javascripts/vizualization.js
@@ -1,1 +1,29 @@
-console.log("SANITY CHECK WERQING")
+console.log("SANITY CHECK WERQING");
+
+var beerType = "English Pale Ale";
+
+$.ajax({
+	type: "GET",
+	url: '/members/beer'
+	dataType: 'json',
+	data: {
+		beer: beerType
+	},
+	success: function (res) {
+		console.log("Found members who like this beer!", res);
+	}
+});
+
+var state = "Minnesota"
+
+$.ajax({
+	type: 'GET',
+	url: '/members/state',
+	dataType: 'json',
+	data: {
+		memberState: state 
+	},
+	success: function(res) {
+		console.log("Found members from this state!", res);
+	}
+});
