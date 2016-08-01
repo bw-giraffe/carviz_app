877e21cc3181259d7e45a0baffcffa4b423a4576
bw-giraffe <theletterrsquared@gmail.com>
2016-08-01 16:41:28 -0700

    sample chart working

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'faker'
 
-gem 'd3-rails'
+gem 'd3_rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 # Use postgresql as the database for Active Record
--- app/assets/javascripts/vizualization.js
+++ app/assets/javascripts/vizualization.js
@@ -2,21 +2,21 @@ console.log("SANITY CHECK WERQING");
 
 var beerType = "English Pale Ale";
 
-$.ajax({
-	type: "GET",
-	url: '/members/beer/',
-	dataType: 'json',
-	data: {
-		memberBeer: beerType
-	},
-	success: function (res) {
-		console.log("Found members who like this beer!", res);
-	},
-	error: function(err) {
-		console.log("There was an error", err)
-	}
-});
-
+// $.ajax({
+// 	type: "GET",
+// 	url: '/members/beer/',
+// 	dataType: 'json',
+// 	data: {
+// 		memberBeer: beerType
+// 	},
+// 	success: function (res) {
+// 		console.log("Found members who like this beer!", res);
+// 		draw(res);
+// 	},
+// 	error: function(err) {
+// 		console.log("There was an error", err)
+// 	}
+// });
 var memState = "Minnesota";
 
 $.ajax({
@@ -28,8 +28,49 @@ $.ajax({
 	},
 	success: function(res) {
 		console.log("Found members from this state!", res);
+		var w = 400;
+var h = 400;
+var r = h/2;
+var color = d3.scale.category20c();
+
+var data = [{"label":"Category A", "value":20}, 
+		          {"label":"Category B", "value":50}, 
+		          {"label":"Category C", "value":30}];
+
+
+var vis = d3.select('#chart').append("svg:svg").data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+var pie = d3.layout.pie().value(function(d){return d.value;});
+
+// declare an arc generator function
+var arc = d3.svg.arc().outerRadius(r);
+
+// select paths, use arc generator to draw
+var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+arcs.append("svg:path")
+    .attr("fill", function(d, i){
+        return color(i);
+    })
+    .attr("d", function (d) {
+        // log the result of the arc generator to show how cool it is :)
+        console.log(arc(d));
+        return arc(d);
+    });
+
+// add the text
+arcs.append("svg:text").attr("transform", function(d){
+			d.innerRadius = 0;
+			d.outerRadius = r;
+    return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+    return data[i].label;}
+		);
 	},
 	error: function(err) {
 		console.log("There was an error", err)
 	}
 });
+
+
+
+
+
+
